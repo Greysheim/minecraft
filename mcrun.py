bfb25f94bb54d835a5bb8c9b8723c562a75c3133
Daniel Cranston <00ff00@gmail.com>
2014-01-02 17:56:26 +1300

    created JarList class

--- mcrun.py
+++ mcrun.py
@@ -22,7 +22,6 @@
 # To do:
 #    Read max_ram, max_worlds from config file
 #        Alternatively, allow manual specification of RAM values
-#    Combine jars and jar_args into a nested list
 #    Implement options: args.level, args.port
 #    Allow script to be called from world directory without WORLD argument
 #    Allow forwarding of args
@@ -42,40 +41,39 @@ if encoding is None: encoding = "utf-8"
 
 
 class IntRange(object):
-    def __init__(self, start, stop=None):
-        if stop is None:
-            start, stop = 0, start
-        self.start, self.stop = start, stop
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
     def __call__(self, value):
         value = int(value)
         if value < self.start or value >= self.stop:
             raise argparse.ArgumentTypeError('value outside of range')
         return value
 
+class JarList(object):
+    def __init__(self, jars):
+        self.j = jars
+        self.names = []
+        for i in self.j:
+            self.names += [i[0]]
+
+
 def run_command(command):
-    p = subprocess.Popen(command.split(),
+    p = subprocess.Popen(command,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT)
     return iter(p.stdout.readline, b'')
 def interact_command(command):
-    for output_line in run_command(command):
-        sys.stdout.write(str(output_line.decode(encoding)))
-def sum_lines_command(command, pattern=None):
-    if pattern is None:
-        return sum(1 for output_line in run_command(command))
-    else:
-        return sum(1 for output_line in run_command(command) 
-                if any(s in output_line.decode(encoding) for s in pattern))
-def str_command(command):
-    for output_line in run_command(command):
-        return (output_line.decode(encoding))
+    for line in run_command(command):
+        sys.stdout.write(str(line.decode(encoding)))
+def running_processes(pattern):
+    return sum(1 for line in run_command(["ps", "x"])
+            if any(s in line.decode(encoding) for s in pattern))
 
 max_worlds = 2
 max_ram = int(400 / max_worlds)  # Allocate 400M RAM total
-# jars = [["minecraft_server.jar", "nogui"], ["craftbukkit.jar", "-o true"]]
-jars = ["minecraft_server.jar", "craftbukkit.jar"]
-jar_args = ["nogui", "-o true"]
-parent_dir = "{0}/minecraft".format(os.getenv("HOME"))
+jars = JarList([["minecraft_server.jar", "nogui"], ["craftbukkit.jar", "-o", "true"]])
+parent_dir = "{}/minecraft".format(os.getenv("HOME"))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("world",
@@ -84,9 +82,12 @@ parser.add_argument("world",
         type=str)
 parser.add_argument("-v", "--verbose",
         help="increase output verbosity",
-        action="store_const",
-        const="v",
-        default="")
+        action="store_true",
+        default=False)
+parser.add_argument("-t", "--test",
+        help="simulate actions. implies -v",
+        action="store_true",
+        default=False)
 parser.add_argument("-b", "--bukkit",
         help="run as bukkit server",
         dest="jar",
@@ -114,43 +115,45 @@ parser.add_argument("-p", "--port",
 args = parser.parse_args()
 
 if args.saveover: args.save = True
+if args.test: args.verbose = True
 world_dir = "{0}/{1}".format(parent_dir, args.world)
 
-worlds_running = sum_lines_command("ps x", jars)
+worlds_running = running_processes(jars.names)
 if worlds_running >= max_worlds:
     sys.exit("{0}: Too many worlds running ({1})\n".format(script_name,
             worlds_running))
 
-to_run = "java -Xincgc -Xms50M -Xmx{0}M -jar {1} {2}".format(max_ram,
-        jars[args.jar], jar_args[args.jar])
+to_run = ["java", "-Xincgc", "-Xms50M", "-Xmx{}M".format(max_ram), "-jar"]
+to_run += jars.j[args.jar]
 
 if args.verbose:
-    print("instances of minecraft server running: {0}".format(worlds_running))
-    print("parent dir: {0}".format(parent_dir))
-    print("world: {0}".format(args.world))
-    print("keep alive: {0}".format(args.keepalive))
-    print("save: {0}".format(args.save))
-    print("save over: {0}".format(args.saveover))
+    print("encoding: {}".format(encoding))
+    print("instances of minecraft server running: {}".format(worlds_running))
+    print("parent dir: {}".format(parent_dir))
+    print("world: {}".format(args.world))
+    print("keep alive: {}".format(args.keepalive))
+    print("save: {}".format(args.save))
+    print("save over: {}".format(args.saveover))
     if args.level is None:
         print("level: (read from server.properties)")
     else:
-        print("level: {0}".format(args.level))
+        print("level: {}".format(args.level))
     if args.port is None:
         print("port: (read from server.properties)")
     else:
-        print("port: {0}".format(args.port))
-    print("current working directory: {0}".format(os.getcwd()))
+        print("port: {}".format(args.port))
+    print("current working directory: {}".format(os.getcwd()))
 
 while True:
     if not os.path.isdir(world_dir):
-        sys.exit("{0}: world directory does not exist".format(script_name))
+        sys.exit("{}: world directory does not exist".format(script_name))
     os.chdir(world_dir)
-    if args.verbose: print("executing: {0}".format(to_run))
-    interact_command(to_run)  # Run server program
+    if args.verbose: print("executing: {}".format(" ".join(to_run)))
+    if not args.test: interact_command(to_run)  # Run server program
     if args.save:
         # Backup world to a tarball in parent_dir/saves/args.world
-        if not os.path.isdir("{0}/saves".format(parent_dir)):
-            os.makedirs("{0}/saves".format(parent_dir))
+        if not os.path.isdir("{}/saves".format(parent_dir)):
+            os.makedirs("{}/saves".format(parent_dir))
         save_dir = "{0}/saves/{1}".format(parent_dir, args.world)
         if not os.path.isdir(save_dir): os.makedirs(save_dir)
         os.chdir(os.pardir)
@@ -166,9 +169,10 @@ while True:
                 save_file = "{0}-{1}.tgz".format(save_base, i)
                 break
             i += 1
-        print("backing up: {0}".format(save_file))
-        with tarfile.open(save_file, "w:gz") as tar:
-            tar.add(world_dir, arcname=os.path.basename(args.world))
+        print("backing up: {}".format(save_file))
+        if not args.test:
+            with tarfile.open(save_file, "w:gz") as tar:
+                tar.add(world_dir, arcname=os.path.basename(args.world))
     if not args.keepalive: break
     print("restarting in 5 seconds (press enter to cancel)")
     i, _, _ = select.select( [sys.stdin], [], [], 5 )
