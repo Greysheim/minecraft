92eab541be9160fb46625dee690d408dcfff2d77
Daniel Cranston <00ff00@gmail.com>
2014-03-30 16:25:18 +1300

    Major reshuffle
    Rewrote some sections of code, moved some sections into functions

--- mcrun.py
+++ mcrun.py
@@ -35,11 +35,7 @@ import select
 import time
 import tarfile
 
-script_name = os.path.basename(__file__)
-encoding = locale.getdefaultlocale()[1]
-if encoding is None: encoding = "utf-8"
-
-
+###### Classes ######
 class IntRange(object):
     def __init__(self, start, stop):
         self.start = start
@@ -52,85 +48,107 @@ class IntRange(object):
 
 class JarList(object):
     def __init__(self, jars):
-        self.j = jars
+        self.jars = jars
         self.names = []
-        for i in self.j:
+        for i in self.jars:
             self.names += [i[0]]
+    def __call__(self, idx):
+        return self.jars[idx]
 
+# class Command(object):
+#    def __init__(self, command):
+#        self.command = command
+#    def execute(self):
+#        run_command(self.command)
 
+###### Static variables ######
+encoding = locale.getdefaultlocale()[1]
+if encoding is None:
+    encoding = "utf-8"
+
+max_worlds = 2
+total_max_ram = 400 # Megabytes
+max_ram = int(total_max_ram / max_worlds)
+jars = JarList([ ["minecraft_server.jar", "nogui"],
+                 ["craftbukkit.jar", "-o", "true"] ])
+java_command = ["java", "-Xms50M", "-Xmx{}M".format(max_ram), "-jar"]
+#java_command += ["-Xincgc"]
+minecraft_dir = "{}/minecraft".format(os.getenv("HOME"))
+save_dir = "{}/saves".format(minecraft_dir)
+# Seconds to wait before restart when args.keepalive option is True
+restart_wait = 5
+
+###### Methods ######
+
+### Command methods ###
 def run_command(command):
     p = subprocess.Popen(command,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT)
     return iter(p.stdout.readline, b'')
+
 def interact_command(command):
     for line in run_command(command):
         sys.stdout.write(str(line.decode(encoding)))
-def running_processes(pattern):
+
+def count_running_processes(pattern):
     return sum(1 for line in run_command(["ps", "x"])
             if any(s in line.decode(encoding) for s in pattern))
 
-max_worlds = 2
-max_ram = int(400 / max_worlds)  # Allocate 400M RAM total
-jars = JarList([["minecraft_server.jar", "nogui"], ["craftbukkit.jar", "-o", "true"]])
-parent_dir = "{}/minecraft".format(os.getenv("HOME"))
+### Argument parser ###
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("world",
+            help="world directory",
+            metavar="WORLD",
+            type=str)
+    parser.add_argument("-v", "--verbose",
+            help="increase output verbosity",
+            action="store_true",
+            default=False)
+    parser.add_argument("-t", "--test",
+            help="simulate actions. implies -v",
+            action="store_true",
+            default=False)
+    parser.add_argument("-b", "--bukkit",
+            help="run as bukkit server",
+            dest="jar",
+            action="store_const",
+            const=1,
+            default=0)
+    parser.add_argument("-k", "--keepalive",
+            help="auto reboot on exit",
+            action="store_true",
+            default=False)
+    parser.add_argument("-s", "--save",
+            help="save world on close",
+            action="store_true",
+            default=False)
+    parser.add_argument("-o", "--saveover",
+            help="overwrite save (else appends number). implies -s",
+            action="store_true",
+            default=False)
+    parser.add_argument("-e", "--level",
+            help="level within WORLD to run (NYI)",
+            type=str)
+    parser.add_argument("-p", "--port",
+            help="listening port (valid range: 1-65535) (NYI)",
+            type=IntRange(1, 65536))
+    return parser
 
-parser = argparse.ArgumentParser()
-parser.add_argument("world",
-        help="world directory",
-        metavar="WORLD",
-        type=str)
-parser.add_argument("-v", "--verbose",
-        help="increase output verbosity",
-        action="store_true",
-        default=False)
-parser.add_argument("-t", "--test",
-        help="simulate actions. implies -v",
-        action="store_true",
-        default=False)
-parser.add_argument("-b", "--bukkit",
-        help="run as bukkit server",
-        dest="jar",
-        action="store_const",
-        const=1,
-        default=0)
-parser.add_argument("-k", "--keepalive",
-        help="auto reboot on exit",
-        action="store_true",
-        default=False)
-parser.add_argument("-s", "--save",
-        help="save world on close",
-        action="store_true",
-        default=False)
-parser.add_argument("-o", "--saveover",
-        help="overwrite save (else appends number). implies -s",
-        action="store_true",
-        default=False)
-parser.add_argument("-e", "--level",
-        help="level within WORLD to run (NYI)",
-        type=str)
-parser.add_argument("-p", "--port",
-        help="listening port (valid range: 1-65535) (NYI)",
-        type=IntRange(1, 65536))
-args = parser.parse_args()
+def parse_args(parser):
+    args = parser.parse_args()
+    if args.saveover:
+        args.save = True
+    if args.test:
+        args.verbose = True
+    return args
 
-if args.saveover: args.save = True
-if args.test: args.verbose = True
-world_dir = "{}/{}".format(parent_dir, args.world)
-
-worlds_running = running_processes(jars.names)
-if worlds_running >= max_worlds:
-    sys.exit("{}: Too many worlds running ({})\n".format(script_name,
-            worlds_running))
-
-to_run = ["java", "-Xms50M", "-Xmx{}M".format(max_ram), "-jar"]
-#to_run += ["-Xincgc"]
-to_run += jars.j[args.jar]
-
-if args.verbose:
+### Print variables method (used in verbose option, for debugging) ###
+def print_variables():
     print("encoding: {}".format(encoding))
     print("instances of minecraft server running: {}".format(worlds_running))
-    print("parent dir: {}".format(parent_dir))
+    print("minecraft dir: {}".format(minecraft_dir))
     print("world: {}".format(args.world))
     print("keep alive: {}".format(args.keepalive))
     print("save: {}".format(args.save))
@@ -145,27 +163,41 @@ if args.verbose:
         print("port: {}".format(args.port))
     print("current working directory: {}".format(os.getcwd()))
 
+###### Main command sequence ######
+
+worlds_running = count_running_processes(jars.names)
+assert worlds_running < max_worlds, "Too many worlds already running"
+
+args = parse_args(create_parser())
+
+# Set argument-dependent variables
+to_run = java_command + jars(args.jar)
+world_dir = "{}/{}".format(minecraft_dir, args.world)
+save_world_dir = "{}/{}".format(save_dir, args.world)
+save_file_base = "{}/{}-{{}}-{{}}.tgz".format(save_world_dir, args.world)
+
+if args.verbose:
+    print_variables()
+
 while True:
-    if not os.path.isdir(world_dir):
-        sys.exit("{}: world directory does not exist".format(script_name))
+    assert os.path.isdir(world_dir), "World directory doesn't exist"
     os.chdir(world_dir)
     if args.verbose: print("executing: {}".format(" ".join(to_run)))
-    if not args.test: interact_command(to_run)  # Run server program
+    if not args.test:
+        interact_command(to_run)  # Run server program
     if args.save:
-        # Backup world to a tarball in parent_dir/saves/args.world
-        if not os.path.isdir("{}/saves".format(parent_dir)):
-            os.makedirs("{}/saves".format(parent_dir))
-        save_dir = "{}/saves/{}".format(parent_dir, args.world)
-        if not os.path.isdir(save_dir): os.makedirs(save_dir)
+        # Backup world to a tarball in minecraft_dir/saves/args.world
+        if not os.path.isdir(save_dir):
+            os.makedirs(save_dir)
+        if not os.path.isdir(save_world_dir):
+            os.makedirs(save_world_dir)
         os.chdir(os.pardir)
-        save_base = "{}/{}-{}-{{}}.tgz".format(save_dir, args.world,
-                time.strftime("%Y-%m-%d"))
+        date = time.strftime("%Y-%m-%d")
         i = 0
         while True:
-            j = i
-            if args.saveover: j += 1
-            if not os.path.exists(save_base.format(j)):
-                save_file = save_base.format(i)
+            next_save_file = save_file_base.format(date, i + args.saveover)
+            if not os.path.exists(next_save_file):
+                save_file = save_file_base.format(date, i)
                 break
             i += 1
         print("backing up: {}".format(save_file))
@@ -173,8 +205,8 @@ while True:
             with tarfile.open(save_file, "w:gz") as tar:
                 tar.add(world_dir, arcname=os.path.basename(args.world))
     if not args.keepalive: break
-    print("restarting in 5 seconds (press enter to cancel)")
-    i, _, _ = select.select( [sys.stdin], [], [], 5 )
+    print("restarting in {} sec (press enter to cancel)".format(restart_wait))
+    i, _, _ = select.select( [sys.stdin], [], [], restart_wait )
     if (i):
         sys.stdin.readline()
         break
